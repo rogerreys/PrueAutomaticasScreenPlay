ede9d4d13f675fbc9166c690e76a1623954a3f9c
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-05-06 11:43:56 -0500

    JCMA Unificación y Eliminación de features

--- pfprt.pfioperation.test.impl/src/test/java/com/cobiscorp/cobis/pfprt/test/impl/steps/BusquedaDepositoStepDefinitions.java
+++ pfprt.pfioperation.test.impl/src/test/java/com/cobiscorp/cobis/pfprt/test/impl/steps/BusquedaDepositoStepDefinitions.java
@@ -1,20 +1,25 @@
 package com.cobiscorp.cobis.pfprt.test.impl.steps;
 
-import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import com.cobiscorp.cobis.pfprt.oprtn.test.AdminAperturaPlazoFijo;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FBloqueosForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FBusquedaClienteForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FBusquedaDepositoForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FCancelacionForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FCancelacionNormalForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FCatalogoForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FCuentasForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FDetalleBusquedaDeposito;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FDetalleOperacionApertura;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FDetalleOperacionForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FFormasDePago;
-import com.cobiscorp.cobis.pfprt.oprtn.test.FRecepcionModalForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FPagoForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FPagoModalForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FRecepcionModalFormManual;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FVistaCancelacionForm;
 import com.cobiscorp.cobis.pfprt.oprtn.test.FVistaOperacionFormManual;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FVistaPagoForm;
+import com.cobiscorp.cobis.pfprt.oprtn.test.FVistaTitularesForm;
 import com.cobiscorp.cobis.serenity.actions.BaseActions;
 import com.cobiscorp.cobis.serenity.actions.ContainerActions;
 import com.cobiscorp.cobis.serenity.actions.FormActions;
@@ -25,7 +30,6 @@ import com.cobiscorp.cobis.serenity.actions.ValidationActions;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
-import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class BusquedaDepositoStepDefinitions {
 	ArrayList<String> nombre  = new ArrayList<String>();
@@ -374,6 +378,128 @@ public class BusquedaDepositoStepDefinitions {
 		HeaderActions.clickAction(FDetalleBusquedaDeposito.Buttons.botonBloqueos);
 		//ValidationActions.isEquals(GBloquear.TextoDetalleOperacion.porcentajeBloqueo, "Disponible - 100.00 %");
 	}
+	
+	//#QA-S471520
+	@Cuando("se presiona el menú acciones, escoger la opción {string}")
+	public void se_presiona_el_menu_acciones_escoger_la_opcion(String opcion){		
+		HeaderActions.clickActionByText(opcion);
+	}
+	
+	//#QA-S471520
+	@Cuando("se debe ingresar los datos en el formulario Bloqueo, presionar el botón Nuevo llenar los campos {string}, {string}, {string}, {string}")	
+	public void se_debe_ingresar_los_datos_en_el_formulario_Bloqueo_presionar_el_boton_Nuevo_llenar_los_campos_motivo_observacion_monto_total(String motivo, String observacion, String monto, String porcentaje){
+		double montoNumerico = 0;
+		GridActions.clickNewRecord(FBloqueosForm.gridListaBloqueos);
+		FormActions.clickOn(FBloqueosForm.Buttons.botonCatalogo);
+		
+		switch (motivo) {
+		case "DETERIORO":
+			GridActions.selectRecord(FCatalogoForm.gridListaCatalogos, 1);
+			break;
+		case "REQUERIMIENTO JUDICIAL":
+			GridActions.selectRecord(FCatalogoForm.gridListaCatalogos, 2);
+			break;
+		case "MIGRACION":
+			GridActions.selectRecord(FCatalogoForm.gridListaCatalogos, 3);
+			break;
+		case "PERDIDA":
+			GridActions.selectRecord(FCatalogoForm.gridListaCatalogos, 4);
+			break;
+		case "ROBO":
+			GridActions.selectRecord(FCatalogoForm.gridListaCatalogos, 5);
+			break;
+		}
+		
+		FormActions.enterText(FBloqueosForm.GridListaBloqueos.observacion, observacion);
+		
+		switch (porcentaje) {
+		case "total":
+			FormActions.enterText(FBloqueosForm.GridListaBloqueos.monto, monto);
+			GridActions.clickAcceptRecord(FBloqueosForm.gridListaBloqueos, 1);
+			FormActions.clickOn(FBloqueosForm.Buttons.botonCerrarBloqueo);
+			break;
+		case "parcial":			
+			montoNumerico = Double.parseDouble(monto) * 0.8; 
+			FormActions.enterText(FBloqueosForm.GridListaBloqueos.monto, String.valueOf(montoNumerico));
+			GridActions.clickAcceptRecord(FBloqueosForm.gridListaBloqueos, 1);
+			FormActions.clickOn(FBloqueosForm.Buttons.botonCerrarBloqueo);
+			break;
+		case "superior":
+			montoNumerico = Double.parseDouble(monto) * 1.2; 
+			FormActions.enterText(FBloqueosForm.GridListaBloqueos.monto, String.valueOf(montoNumerico));
+			GridActions.clickAcceptRecord(FBloqueosForm.gridListaBloqueos, 1);
+			break;
+		}
+		
+		BaseActions.takeScreenshot();		
+	}
+	
+	//#QA-S471520
+	@Cuando("se genera el bloqueo del Certificado de Depósito por el {string} {string}")
+	public void se_genera_el_bloqueo_del_Certificado_de_Deposito_por_el_monto_total(String monto, String porcentaje){
+		switch (porcentaje) {
+		case "total":
+			ValidationActions.isEquals(FDetalleOperacionForm.ResumenInformacion.montoBloqueado, monto);
+			BaseActions.takeScreenshot();
+			break;
+		case "parcial":
+			ValidationActions.isEquals(FDetalleOperacionForm.ResumenInformacion.montoBloqueado, monto);
+			BaseActions.takeScreenshot();
+			break;
+		case "superior":
+			ValidationActions.isEquals(FBloqueosForm.MensajeExcepcion.mensajeInformacion, "Excepción de negocio: 16 - 141069 - [BL_retencion] Monto a bloquear mayor que monto del plazo fijo");
+			BaseActions.takeScreenshot();
+			break;
+		}
+	}
+	
+	//Inicio Cancelacion Certificado
+	@Cuando("presiona el botón siguientes")
+	public void presiona_el_botón_siguientes() {
+	    FormActions.clickOn(FVistaTitularesForm.Buttons.cmdCommand2IdCMTPFPRTMQFFB);
+	}
+	
+	@Cuando("diligencia el formulario de Cancelación {string},{string},{string},{string},{string},{string},{string},{string}")
+	public void diligencia_el_formulario_de_Cancelación(String capital, String saldo, String impuesto, String total, String interesGanado, String interesPagado, String solicitante, String observacion) {
+	    if(capital.length()>0){ FormActions.enterText(FCancelacionForm.DetalleOperacion.monto, capital); }
+	    if(saldo.length()>0){ FormActions.enterText(FCancelacionForm.DetalleOperacion.interesGanado, saldo); }
+	    if(impuesto.length()>0){ FormActions.enterText(FCancelacionForm.DetalleCancelacion.impuesto, impuesto); }
+	    if(total.length()>0){ FormActions.enterText(FCancelacionForm.DetalleCancelacion.montoCancelacion, total); }
+	    if(interesGanado.length()>0){ FormActions.enterText(FCancelacionForm.DetalleOperacion.totalInteresGanado, interesGanado); }
+	    if(interesPagado.length()>0){ FormActions.enterText(FCancelacionForm.DetalleOperacion.totalInteresPagado, interesPagado); }
+	    if(solicitante.length()>0){ FormActions.selectByText(FCancelacionForm.DetalleCancelacion.clienteSolicitante, solicitante); }
+	    if(observacion.length()>0){ FormActions.enterText(FCancelacionForm.DetalleCancelacion.observacion, observacion); }
+	    FormActions.clickOn(FVistaCancelacionForm.Buttons.cmdCommand1IdCMTPFPRTXO7N);
+	}
+	
+	@Cuando("llena el formulario de forma de pago {string} a {string},{string},{string} con {string}")
+	public void llena_el_formulario_de_forma_de_pago_a_con(String formaRecepcion, String beneficiario, String columna, String cuenta, String montoPago) {
+		GridActions.clickNewRecord(FPagoForm.gridListaDetallePagos);
+		FormActions.selectByText(FPagoModalForm.listaDetallePagos.codigoFormaPago, formaRecepcion);
+		if(formaRecepcion.equals("CUENTA DE AHORROS")){
+			FormActions.enterText(FPagoModalForm.listaDetallePagos.monto, montoPago);
+			FormActions.selectByText(FPagoModalForm.listaDetallePagos.cliente, beneficiario);
+			FormActions.clickTextButton(FPagoModalForm.listaDetallePagos.cuentaReferencia);
+			GridActions.selectRecordByColumnAndText(FCuentasForm.gridListaCuentas, Integer.parseInt(columna), cuenta);
+		}else{
+			FormActions.enterText(FPagoModalForm.listaDetallePagos.monto, montoPago);
+			FormActions.selectByText(FPagoModalForm.listaDetallePagos.cliente, beneficiario);
+		}
+		FormActions.clickOn(FPagoModalForm.Buttons.cmdCommand1IdCMTPFPRTMB11);
+		FormActions.clickOn(FVistaPagoForm.Buttons.cmdCommand3IdCMTPFPRTYP273);
+	}
+	
+	@Entonces("el sistema despliega un mensaje informativo indicando que la cuenta se encuentra {string}")
+	public void el_sistema_despliega_un_mensaje_informativo_indicando_que_la_cuenta_se_encuentra(String string) {
+		BaseActions.takeScreenshot();
+	}
+	@Entonces("en la pantalla emergente de los números de cuenta no se visualiza la cuenta cerrada")
+	public void en_la_pantalla_emergente_de_los_números_de_cuenta_no_se_visualiza_la_cuenta_cerrada() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Fin Cancelación Certificado
 
 }
 
